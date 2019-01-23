a4ce635ef9bc09d61624ed107ade45a796199f16
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-01-23 20:20:37 +0800

    update pom.xml file info

--- src/main/scala/com/laibao/scala/cookbook/chapter02/LargeNumber.scala
+++ src/main/scala/com/laibao/scala/cookbook/chapter02/LargeNumber.scala
@@ -1,5 +1,6 @@
 package com.laibao.scala.cookbook.chapter02
 
+import scala.util.Random
 object LargeNumber {
 
   def main(args: Array[String]): Unit = {
@@ -15,5 +16,33 @@ object LargeNumber {
     val d = c toInt
 
     println(s"d is $d")
+
+
+    val random = new Random
+
+    random.nextInt
+
+    random.nextBoolean
+
+    random.nextLong
+
+    random.nextFloat
+
+    random.nextDouble
+
+    random.nextInt(100)
+
+    random.nextPrintableChar
+
+
+    val intRange = 0 to new Random().nextInt(100)
+    intRange.foreach(println)
+
+
+
+    val range = for (i <- 0 to random.nextInt(1000)) yield i * 2
+
+    range.foreach(println)
+
   }
 }
